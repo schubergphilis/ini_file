c28c51150ae077589f19af43b417ad09a0278087
Gert Kremer <gkremer@schubergphilis.com>
2016-12-14 15:12:05 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# inifile
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'inifile'
+maintainer 'The Authors'
+maintainer_email 'gkremer@schubergphilis.com'
+license 'apachev2'
+description 'Installs/Configures inifile'
+long_description 'Installs/Configures inifile'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/inifile/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/inifile' if respond_to?(:source_url)
